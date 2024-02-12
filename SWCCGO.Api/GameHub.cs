9b0894d2b55ac70272e8c11450c4738aa8b93318
Nick Heinbaugh <nick.heinbaugh@live.com>
2024-02-12 13:06:24 -0800

    initial commit

---
+++ SWCCGO.Api/GameHub.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.SignalR;
+
+namespace SWCCGO.Api;
+
+public class GameHub: Hub
+{
+    public async Task Send(string name, string message)
+    {
+        await Clients.All.SendAsync("broadcastMessage", name, message);
+    }
+}
---
+++ SWCCGO.Models/Card.cs
@@ -0,0 +1,6 @@
+﻿namespace SWCCGO.Models;
+
+public class Class1
+{
+    public Guid CardId { get; set; }
+}
